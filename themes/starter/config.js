0a6ebc78396885e4871e54645a6fc8f8e8c78919
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2024-03-18 22:20:34 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -11,14 +11,14 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
+  STARTER_HERO_TITLE_1: 'BUZZ Blog 门户网站主题预览', // 英雄区文字
   STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+  STARTER_HERO_BUTTON_1_TEXT: 'BUZZ主页', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'https://home.buzzchat.top/', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: 'BUZZAI集', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: 'https://blog.buzzchat.top/article/ai', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标  /images/starter/github.svg  ，不需要则留空
 
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
