cf49aeacba4f4fc8079e89a73ca752f0e6e34918
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2024-06-25 13:12:32 +0800

    test

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -37,20 +37,23 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
         }
       `}</style>
 
-      <div
-        className={`${isDarkMode ? 'bg-[#CA8A04]' : 'bg-[#0060e0]'} absolute top-0 w-full h-full py-10 flex justify-center items-center`}>
-        {/* 文章背景图 */}
         <div
-          id='post-cover-wrapper'
-          style={{
-            filter: 'blur(15px)'
-          }}
-          className='coverdiv lg:opacity-50 lg:translate-x-96 lg:rotate-12'>
-          <LazyImage
-            id='post-cover'
-            className='w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[20rem]'
-            src={headerImage}
-          />
+          className={`${
+            isDarkMode ? 'bg-[#CA8A04]' : 'bg-[#0060e0]'
+          } absolute top-0 w-full h-full py-10 flex justify-center items-center`}
+        >
+          {/* 文章背景图 */}
+          <div
+            id="post-cover-wrapper"
+            className="coverdiv w-full h-full"
+            style={{ filter: 'blur(5px)', opacity: '0.8' }}
+          >
+            <LazyImage
+              id="post-cover"
+              className="w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[20rem]"
+              src={headerImage}
+            />
+          </div>
         </div>
 
         {/* 文章文字描述 */}
