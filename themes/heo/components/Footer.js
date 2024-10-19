89e796f2acf2c8bb6193c3bd5ebb0f0c3515d4f7
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2024-10-19 22:37:10 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,6 +1,3 @@
-import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
-
 const Footer = ({ title }) => {
   const d = new Date()
   const currentYear = d.getFullYear()
@@ -9,11 +6,11 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
+    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
       <div
         id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
+        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
       />
 
       {/* 社交按钮 */}
@@ -22,6 +19,15 @@ const Footer = ({ title }) => {
       </div>
 
       <br />
+
+      {/* 在这里增加图片 */}
+      <div id='footer-image' className='w-full h-36'>
+        <img 
+          src="https://p.qjq.cn/cover/202410191400532.png" 
+          alt="Footer Image" 
+          className='w-full h-full object-cover'
+        />
+      </div>
 
       {/* 底部页面信息 */}
       <div
@@ -56,8 +62,6 @@ const Footer = ({ title }) => {
             <i className='fas fa-users' />{' '}
             <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
           </span>
-
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
         </div>
       </div>
     </footer>
